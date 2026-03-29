3a09681e7af1fc17a11bec57bf9697d6a15daa5c
RafkaJulianee <rafkagenggkk@gmail.com>
2026-03-29 19:57:45 +0700

    up

--- index.php
+++ index.php
@@ -1158,4 +1158,12 @@ while ($row = $stmt->fetch()) {
   <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></scrip
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
